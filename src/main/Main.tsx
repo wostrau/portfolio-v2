a61b09d69ec8f1524d705ea1fff48903942a657a
wostrau <a.ostrovtsov@gmail.com>
2023-02-19 23:47:43 +0100

    3rd part of portfolio layout - TBD (stopped at 34:30)

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import styles from './Main.module.scss';
+import {ParticlesTS} from '../common/components/particles/ParticlesTS';
 
 export const Main = () => {
+
     return (
         <div className={styles.mainBlock}>
+            <ParticlesTS/>
             <div className={styles.container}>
-                <div className={styles.text}>
+                <div className={styles.greeting}>
                     <span>Hi There</span>
-                    <h1>I am Ales Astrautsou</h1>
-                    <p>a REACT developer</p>
+                    <span>I am Ales <span>Astrautsou</span></span>
+                    <h1>REACT developer.</h1>
                 </div>
                 <div className={styles.photo}>
-
+                    <div className={styles.image}></div>
                 </div>
             </div>
         </div>
